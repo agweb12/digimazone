06e47130bfa247126d636dfae1d174804b83f824
agweb12 <151913740+agweb12@users.noreply.github.com>
2024-11-28 17:00:07 +0100

    version 1.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuCache = document.querySelector(".menuCache");
     const shopCategorie = document.querySelector(".shopCategorie");
+    let transitioning = false;
 
     function apparitionGrosMenu() {
-        if (menuCache.classList.contains('is-visible')) {
-            menuCache.classList.remove('is-visible');
-        } else {
-            menuCache.classList.add('is-visible');
-        }
-    }
-
+        if(!transitioning){
+            transitioning = true;
+            if (menuCache.classList.contains('is-visible')) {
+                menuCache.classList.remove('is-visible');
+            } else {
+                menuCache.classList.add('is-visible');
+            };
+            setTimeout(() => {
+                transitioning = false; // Fin de la transition après 500 ms
+            }, 300);
+        };
+        
+    };
 
     shopCategorie.addEventListener('click', apparitionGrosMenu);
 
+
+    // const CHAT = "Simba";
+
+    // let chien = CHAT;
+    // console.log(typeof chien, chien);
 });
