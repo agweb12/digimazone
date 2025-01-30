800996c3cc72dc802c50b289eb448ed59bfdf689
agweb12 <graziani1112@gmail.com>
2025-01-30 20:43:58 +0100

    version 1.2.3

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,20 @@ document.addEventListener('DOMContentLoaded', function () {
     let transitioning = false;
 
     function apparitionGrosMenu() {
-        if(!transitioning){
+        if (!transitioning) {
             transitioning = true;
             if (menuCache.classList.contains('is-visible')) {
                 menuCache.classList.remove('is-visible');
             } else {
                 menuCache.classList.add('is-visible');
-            };
+            }
             setTimeout(() => {
                 transitioning = false; // Fin de la transition après 500 ms
             }, 300);
-        };
-        
-    };
+        }
+
+    }
 
     shopCategorie.addEventListener('click', apparitionGrosMenu);
 
-
-    // const CHAT = "Simba";
-
-    // let chien = CHAT;
-    // console.log(typeof chien, chien);
 });
