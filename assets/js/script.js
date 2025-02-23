4b54dcbd310fc478b67347d5ecb5b05da663efa5
agweb12 <graziani1112@gmail.com>
2025-02-23 15:04:44 +0100

    version 1.2.5 - Mode Clair et Sombre

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuCache = document.querySelector(".menuCache");
     const shopCategorie = document.querySelector(".shopCategorie");
     let transitioning = false;
+    let html = document.querySelector("html");
+    let switchMode = document.querySelector(".buttonSwitch");
 
     function apparitionGrosMenu() {
         if (!transitioning) {
@@ -17,6 +19,29 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
     }
+
+    function chargementJeuCouleur() {
+        if (window.matchMedia("color-scheme: light")) {
+            html.classList.add("light");
+        } else if (window.matchMedia("color-scheme: dark")) {
+            html.classList.add("dark");
+            switchMode.classList.add("switch");
+        }
+    }
+
+    function changementMode() {
+        switchMode.classList.toggle('switch');
+        if (switchMode.classList.contains('switch')) {
+            html.classList.remove('light');
+            html.classList.add('dark');
+        } else {
+            html.classList.remove('dark');
+            html.classList.add('light');
+        }
+    }
+
+    window.addEventListener('load', chargementJeuCouleur);
+    switchMode.addEventListener('click', changementMode);
 
     shopCategorie.addEventListener('click', apparitionGrosMenu);
 
